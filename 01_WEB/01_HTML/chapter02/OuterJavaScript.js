8c73424cb58b4a9d39ab332077b509566f57ffee
jungyeseul/git <jys033022@naver.com>
2024-07-31 09:10:16 +0900

    HTML/CSS/JS

---
+++ 01_WEB/01_HTML/chapter02/OuterJavaScript.js
@@ -0,0 +1,2 @@
+// 경고창 출력하는 방법
+alert('Hello JavaScript...!');
---
+++ 01_WEB/03_JS/chapter10/defer.js
@@ -0,0 +1,4 @@
+// h1태그의 배경색상 변경
+document.querySelector('h1').style.backgroundColor = 'red';
+// h1태그의 글자색상 변경
+document.querySelector('h2').style.color = 'red';
